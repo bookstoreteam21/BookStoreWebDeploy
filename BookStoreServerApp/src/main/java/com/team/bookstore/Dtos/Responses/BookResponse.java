26252fd7f104ed788e5279ff8f66ff6b32917f7d
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-05-31 13:37:58 +0700

    Fix

--- BookStoreServerApp/src/main/java/com/team/bookstore/Dtos/Responses/BookResponse.java
+++ BookStoreServerApp/src/main/java/com/team/bookstore/Dtos/Responses/BookResponse.java
@@ -34,7 +34,7 @@ public class BookResponse {
     Date updateAt;
     String createBy;
     String updateBy;
-    int catgory_id;
+    int category_id;
     Set<Author> authors;
     Publisher publisher;
     Language language;
