adcdfd98b1ef29356c8c09b2cab77d18ed11b4c4
HoangLam21 <hoanglamdeveloper@gmail.com>
2024-06-07 11:48:51 +0700

    TestSetCredentials

--- BookStoreServerApp/src/main/java/com/team/bookstore/Configs/CorsConfig.java
+++ BookStoreServerApp/src/main/java/com/team/bookstore/Configs/CorsConfig.java
@@ -20,6 +20,7 @@ public class CorsConfig {
         corsConfig.addAllowedOriginPattern("*");
         corsConfig.addAllowedHeader("*");
         corsConfig.addAllowedMethod("*");
+        corsConfig.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
 
